05067b2e5545b7d28166c7d62b79759ac1a0e887
work.alexeevs@gmail.com <work.alexeevs@gmail.com>
2023-07-20 21:27:57 +0300

    fix storybook

--- config/storybook/decorators/router.tsx
+++ config/storybook/decorators/router.tsx
@@ -1,7 +1,7 @@
 import { StoryFn } from "@storybook/react";
 import { BrowserRouter } from "react-router-dom";
 
-export const RouterDecorator = (Story: () => StoryFn) => (
+export const RouterDecorator = (Story: StoryFn) => (
   <BrowserRouter>
     <Story />
   </BrowserRouter>
