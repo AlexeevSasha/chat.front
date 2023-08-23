1a9dacc25542862fb3adfd2442c347a80f628c89
work.alexeevs@gmail.com <work.alexeevs@gmail.com>
2023-08-23 20:01:34 +0300

    add socket

--- src/common/components/ContextProvider.tsx
+++ src/common/components/ContextProvider.tsx
@@ -1,11 +1,13 @@
 import { FC, createContext, ReactElement } from "react";
 import { useTheme } from "@/common/hooks/useTheme";
+import { useSocket } from "@/common/hooks/useSocket";
 
-type ContextType = ReturnType<typeof useTheme>;
+type ContextType = ReturnType<typeof useTheme> & ReturnType<typeof useSocket>;
 
 export const AppContext = createContext<ContextType>({} as ContextType);
 
 export const ContextProvider: FC<{ children: ReactElement }> = ({ children }) => {
   const theme = useTheme();
-  return <AppContext.Provider value={{ ...theme }}>{children}</AppContext.Provider>;
+  const { socket } = useSocket();
+  return <AppContext.Provider value={{ ...theme, socket }}>{children}</AppContext.Provider>;
 };
